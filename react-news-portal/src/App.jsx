edeac4067df7af34e037d57fc4d88c17f7263f7d
Safeer <safeeralam997@gmail.com>
2024-06-19 17:43:51 +0530

    initial folder setup

---
+++ react-news-portal/src/App.jsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>react news portal</h1>
+    </div>
+  );
+}
+
+export default App;
